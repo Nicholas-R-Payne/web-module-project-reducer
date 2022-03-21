a0cfb028147d5a0e8a7014e36701257943443d88
DarkedgeTH <darkedgeth@gmail.com>
2022-03-21 14:35:22 -0400

    imported and initialized changeOperation

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers'
-import { applyNumber } from '../actions'
+import { applyNumber, changeOperation } from '../actions'
 
 function App() {
   const [state, dispatch ] = useReducer(reducer, initialState)
 
   const handleNumClick = number => {
     dispatch(applyNumber(number))
+  }
+
+  const handleOperationClick = operator => {
+    dispatch(changeOperation(operator))
   }
   
   return (
@@ -56,7 +60,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
+              <CalcButton value={"+"} onClick={() => handleOperationClick('+')}/>
               <CalcButton value={"*"}/>
               <CalcButton value={"-"}/>
             </div>
